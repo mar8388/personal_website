16cbb280918e873e536ee9ae14c6b2b4041f8a58
George Jose <gejose@deloitte.ca>
2016-02-16 17:14:29 -0500

    djkhaled

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 		result = $('#main .content .row .item div img').attr('src');
 		console.log($('#main .content .row .item div img').attr('src'));
 		console.log('Posting to slack');
-		request('https://hooks.slack.com/services/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', {'json': {'text': result}}, function(err, res, bod){
+		request('https://hooks.slack.com/services/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', {'json': {'text': 'TEST'}}, function(err, res, bod){
 			if (!err)
 				console.log('posted to webhook');
 			else 
